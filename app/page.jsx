f3fc6b9679c93d547312c340eac30f1b8e82be3d
Sarhan41 <sp.webdev2024@gmail.com>
2023-12-04 09:05:05 +0530

    first comit

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ import {
 
 export default function Home() {
   return (
-    <main className="relative z-0 ">
+    <main className="relative z-0 w-full">
       <DisplayBlocker />
       <section className="bg-cover bg-no-repeat bg-center  gradient-1    ">
         <Navbar />
