97537a18222a398990119c8014a2b20c014f2c00
Sarhan41 <sp.webdev2024@gmail.com>
2023-12-04 07:59:23 +0530

    /

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,7 +11,6 @@ export default function RootLayout({ children }) {
       <head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
-{/* // */}
 
       </head>
       <body className="dark:bg-black dark:text-white">{children}</body>
