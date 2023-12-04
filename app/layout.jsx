1c08fed1a4d560e9a62db4e4b5766d3a1f01ae6b
Sarhan41 <sp.webdev2024@gmail.com>
2023-12-04 09:58:17 +0530

    first commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
       <head>
       <meta
   name="viewport"
-  content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no"
+  content="width=device-width, initial-scale=1, maximum-scale=1,  "
 />
 
       </head>
