d628cf73b6bd1d87a8db83b09499d3ec43432f8e
Sarhan41 <sp.webdev2024@gmail.com>
2023-12-04 08:26:19 +0530

    first commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,7 +10,6 @@ export default function RootLayout({ children }) {
     <html lang="en" className="dark">
       <head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-{/* demo commit */}
       </head>
       <body className="dark:bg-black dark:text-white">{children}</body>
     </html>
