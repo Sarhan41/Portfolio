63b6a7e82fbbfe678fbaad3e419608bd8924e10a
Sarhan41 <sp.webdev2024@gmail.com>
2023-12-04 12:26:42 +0530

    first commit

--- app/page.jsx
+++ app/page.jsx
@@ -10,22 +10,22 @@ import {
 
 export default function Home() {
   return (
-    <main className="relative z-0 h-full w-full">
+    <main className="relative z-0 h-fit w-full m-0 p-0">
       <DisplayBlocker />
-      <section className="bg-cover bg-no-repeat bg-center  gradient-1    h-fit">
+      <section className="bg-cover bg-no-repeat bg-center  gradient-1    min-h-screen">
         <Navbar />
         <HeroWrapper />
       </section>
-      <section className="bg-cover bg-no-repeat bg-center  gradient-2  h-fit ">
+      <section className="bg-cover bg-no-repeat bg-center  gradient-2  min-h-screen ">
         <AboutWrapper />
       </section>
-      <section className="bg-cover bg-no-repeat bg-center  gradient-1   h-fit">
+      <section className="bg-cover bg-no-repeat bg-center  gradient-1   min-h-screen">
         <SkillsWrapper />
       </section>
-      <section className="bg-cover bg-no-repeat bg-center  gradient-2 h-fit  ">
+      <section className="bg-cover bg-no-repeat bg-center  gradient-2 min-h-screen  ">
         <ProjectsWrapper />
       </section>
-      <section className="bg-cover bg-no-repeat bg-center   gradient-1 h-fit">
+      <section className="bg-cover bg-no-repeat bg-center   gradient-1 min-h-screen">
         <ContactWrapper />
       </section>
     </main>
