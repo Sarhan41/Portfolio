0ec88ae0ca17f53b79129727578d99eaacef718b
Sarhan41 <sp.webdev2024@gmail.com>
2023-12-06 12:24:59 +0530

    first commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
       <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=no" />
 
 
       </head>
