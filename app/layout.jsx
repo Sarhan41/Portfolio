ffdaa8be0d8ec57dd78970d8fc30b337f646d08b
Sarhan41 <sp.webdev2024@gmail.com>
2024-04-07 11:55:40 +0530

    Update lang attribute in RootLayout component

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,7 +7,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="">
       <head>
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
 
